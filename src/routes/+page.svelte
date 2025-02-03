f4ccc2c32cc8a934e4ce9ee723a828f4a4644dbd
Andreas Gajdosik <andreas@gajdosik.org>
2025-02-03 11:40:13 +0100

    Add some text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,19 +44,63 @@
 
 </script>
 
-<h1>Cardz</h1>
+<h1>NoneCard</h1>
 
+<p>
+  NoneCard allows you to share customer cards in the community.
+  You can easily and freely access and use the cards.
+  And make it harder for companies to analyze anything meaningul from the stolen data.
+  Customers of the world, unite!
+</p>
+
+<details id="manifesto">
+  <summary>More info:</summary>
+  <h2>NoneCard Manifesto</h2>
+  <p>
+  Customer cards promise discounts, but in reality they just track the customers.
+  They intrude our privacy and suck data about our purchasing behaviour into clouds somewhere in a sec haven.
+  Customer cards are just a tool of surveillance capitalism.
+  No longer we are customers, we are tagged herds.
+  </p>
+  <p>
+  As if that wasn't shitty enough, it's also just extremely annoying to carry around customer cards.
+  To keep them. Search them in a wallet. To keep track of them.
+  </p>
+  <p>
+  And if you can't find a customer card, or simply don't have one, you get fined the "regular" price, which is out of touch with reality.
+  We are human beings, we have our dignity and it is our right not to be stalked.
+  </p>
+
+  <p>
+  Share and use cards to get the discount while contaminating the extracted data with random noise.
+  Let's obfuscate the data of others and let the others to obfuscate yours.
+  Once the card purchases meat in Prague, then seconds later it makes a vegan purchase in Ostrava.
+  Nonsense to the companies, value to the people!
+  </p>
+</details>
+
+
+
+<h3>Add your customer card</h3>
+<p>
+If you want to add your customer card, send its picture to: <a href="mailto:nonecard@gajdosik.org">nonecard@gajdosik.org</a>.<br>
+(You can also join development of this site at: <a href="https://github.com/agajdosi/nonecard">github.com/agajdosi/nonecard</a>).
+</p>
+
+<h2>Available Cards</h2>
 {#if error}
-    <p class="error">Failed to load records: {error}</p>
+  <p class="error">Failed to load records: {error}</p>
 {:else}
-    <ul>
-        {#each records as record}
-            <li>
-                <h2>{record.company} - {record.type}: {record.code}</h2>
-                <img use:barcode={{ code: record.code, type: record.type }} alt="Barcode for {record.code}"/>
-            </li>
-        {/each}
-    </ul>
+  <ul>
+    {#each records as record}
+      {#if record.devel === false || window.location.hostname === 'localhost'}
+      <li>
+        <h2>{record.company} - {record.name} ({record.country})</h2>
+        <img use:barcode={{ code: record.code, type: record.type }} alt="Barcode for {record.code}"/>
+      </li>
+      {/if}
+    {/each}
+  </ul>
 {/if}
 
 
